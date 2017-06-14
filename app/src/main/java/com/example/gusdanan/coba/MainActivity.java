ab95988befc631ebdaf5b5c14d86d04633edc43a
iamgusdanan <idabagusdananjaya.ibd@gmail.com>
2017-06-14 20:46:43 +0800

    coba bru

--- app/src/main/java/com/example/gusdanan/coba/MainActivity.java
+++ app/src/main/java/com/example/gusdanan/coba/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Coba", "OnCreate");
+        Log.d("Coba", "OnCreateBaru");
 
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.pass);
